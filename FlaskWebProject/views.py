a8943cea70f1e575071e3dc7193eb48dfb6c601b
phduong21 <phduong.bk@gmail.com>
2023-10-07 00:05:33 +0700

    udacity

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -66,8 +66,10 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
+            app.logger.warning('wrong username or password, {}'.format(form.username.data))
             flash('Invalid username or password')
             return redirect(url_for('login'))
+        app.logger.warning('login successful!')
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
@@ -111,18 +113,22 @@ def logout():
     return redirect(url_for('login'))
 
 def _load_cache():
-    # TODO: Load the cache from `msal`, if it exists
-    cache = None
+    cache = msal.SerializableTokenCache()
+    if session.get("token_cache"):
+        cache.deserialize(session["token_cache"])
     return cache
 
 def _save_cache(cache):
-    # TODO: Save the cache, if it has changed
-    pass
+    if cache.has_state_changed:
+        session["token_cache"] = cache.serialize()
 
 def _build_msal_app(cache=None, authority=None):
-    # TODO: Return a ConfidentialClientApplication
-    return None
+    return msal.ConfidentialClientApplication(
+        Config.CLIENT_ID, authority=Config.AUTHORITY,
+        client_credential=Config.CLIENT_SECRET, token_cache=cache)
 
 def _build_auth_url(authority=None, scopes=None, state=None):
-    # TODO: Return the full Auth Request URL with appropriate Redirect URI
-    return None
+    return _build_msal_app().get_authorization_request_url(
+        scopes,
+        state=state,
+        redirect_uri=url_for("authorized", _external=True))
